f32143bdef9639df68930786ad76c3ca35dbac1a
Michelk55 <123950145+Michelk55@users.noreply.github.com>
2023-08-13 14:57:45 -0300

    .

--- exercicio1.py
+++ exercicio1.py
@@ -6,21 +6,25 @@
 
 # Crie a função que será avaliada no exercício aqui
 
+def conta_palavras_unicas(frase):
+    # Convertendo todas as letras para minúsculas para considerar palavras iguais independentemente de caixa
+    frase = frase.lower()
+    
+    # Dividindo a frase em palavras usando espaço como separador
+    palavras = frase.split()
+    
+    # Usando um conjunto para armazenar palavras únicas
+    palavras_unicas = set(palavras)
+    
+    # Retornando a quantidade de palavras únicas
+    return len(palavras_unicas)
 
-
-
+# Exemplo de uso
+frase_exemplo = "Esta é uma frase de exemplo. Uma frase curta para testar a função."
+quantidade_palavras_unicas = conta_palavras_unicas(frase_exemplo)
+print("Quantidade de palavras únicas:", quantidade_palavras_unicas)
 
 
 
 # Teste a sua função aqui (caso ache necessário)
-
-
-
-
-
-
-
-
-
-
 
--- exercicio2.py
+++ exercicio2.py
@@ -6,21 +6,39 @@
 
 # Crie a função que será avaliada no exercício aqui
 
+def is_anagram(str1, str2):
+    # Removendo espaços em branco e convertendo para letras minúsculas para normalizar as strings
+    str1 = str1.replace(" ", "").lower()
+    str2 = str2.replace(" ", "").lower()
+    
+    # Verificando se as strings têm o mesmo comprimento após a remoção de espaços
+    if len(str1) != len(str2):
+        return False
+    
+    # Usando dicionários para contar a ocorrência de cada letra nas duas strings
+    count_dict1 = {}
+    count_dict2 = {}
+    
+    for char in str1:
+        count_dict1[char] = count_dict1.get(char, 0) + 1
+        
+    for char in str2:
+        count_dict2[char] = count_dict2.get(char, 0) + 1
+        
+    # Verificando se os dicionários de contagem são iguais
+    return count_dict1 == count_dict2
 
+# Exemplos de uso
+print(is_anagram("Amor", "Roma"))  # True
+print(is_anagram("Pedra", "Padre"))  # True
+print(is_anagram("Perda", "Pedra"))  # True
+print(is_anagram("Hello", "World"))  # False
 
 
 
 
 
 # Teste a sua função aqui (caso ache necessário)
-
-
-
-
-
-
-
-
 
 
 
